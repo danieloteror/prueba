b37f7b1cbb7bb932007e6835eecaf1f29c5ff49c
Daniel Otero <daniotero13@gmail.com>
2022-11-15 17:18:55 +0100

    root commit

---
+++ src/prueba/prueba.java
@@ -0,0 +1,16 @@
+package prueba;
+
+public class prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 1;
+		while (num>=1 && num<=100 ) {
+			System.out.println(num);
+			num ++;
+			
+		}
+
+	}
+
+}
